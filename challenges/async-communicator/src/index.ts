11ce603e7f3b76fe5ae35b36c8ca83fb742718c2
John Hunter <john.hunter@apperio.com>
2023-06-02 17:09:43 +0100

    Complete async-communicator exercise

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,7 +1,13 @@
 import { expectType } from "tsd";
 
 // IMPLEMENT THIS TYPE
-export type WrapForPenpal<T> = any;
+export type WrapForPenpal<T> = {
+  [K in keyof T]: T[K] extends { (...arg: infer P): infer R }
+    ? R extends Promise<any>
+      ? (...arg: P) => R // already a promise
+      : (...arg: P) => Promise<R>
+    : T[K]; // non-methods
+};
 
 /**
  * Test Scenario - Do not change anything below this line
@@ -12,6 +18,9 @@ const methods = {
   },
   subtract(a: number, b: number): number {
     return a - b;
+  },
+  doAsyncAdd(a: number, b: number): Promise<number> {
+    return Promise.resolve(a + b);
   },
 };
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
@@ -25,3 +34,6 @@ let subtractPromise = asyncMethods.subtract(1, 2);
 expectType<Promise<number>>(subtractPromise);
 // @ts-expect-error
 expectType<typeof subtractPromise>("this should fail");
+
+let doAsyncPromise = asyncMethods.doAsyncAdd(1, 2);
+expectType<Promise<number>>(doAsyncPromise);
